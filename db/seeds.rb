bb8e81b3ad601b02dab13d149d672b5974ddc036
Marcelo Soares Souza <marcelo@itbio3.org>
2017-08-31 15:18:27 -0300

    Correção no Seed para criação de SLUG

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,31 +6,31 @@ usuario_itbio3 = Usuario.new(nome: "ITBio3", email: "contact@itbio3.org", passwo
 usuario_itbio3.skip_confirmation!
 usuario_itbio3.save!
 
-TipoOrganizacao.new(nome: "Organização Não Governamental").save(validate: false)
-TipoOrganizacao.new(nome: "Governamental").save(validate: false)
-TipoOrganizacao.new(nome: "Privada").save(validate: false)
+TipoOrganizacao.new(nome: "Organização Não Governamental", usuario_id: 1).save!
+TipoOrganizacao.new(nome: "Governamental", usuario_id: 1).save!
+TipoOrganizacao.new(nome: "Privada", usuario_id: 1).save!
 
-TemaExperienciaAgroecologica.new(nome: "Agricultura Urbana").save(validate: false)
-TemaExperienciaAgroecologica.new(nome: "Alimentação").save(validate: false)
-TemaExperienciaAgroecologica.new(nome: "Saúde e plantas medicinais").save(validate: false)
-TemaExperienciaAgroecologica.new(nome: "Desenvolvimento Rural").save(validate: false)
-TemaExperienciaAgroecologica.new(nome: "Desenvolvimento Local").save(validate: false)
-TemaExperienciaAgroecologica.new(nome: "Educação").save(validate: false)
-TemaExperienciaAgroecologica.new(nome: "Pesquisa Agrícola e Extensão Rural").save(validate: false)
-TemaExperienciaAgroecologica.new(nome: "Manejo de Recursos Hídricos").save(validate: false)
-TemaExperienciaAgroecologica.new(nome: "Certificação").save(validate: false)
-TemaExperienciaAgroecologica.new(nome: "Comercialização").save(validate: false)
-TemaExperienciaAgroecologica.new(nome: "Processamento e beneficiamento da produção").save(validate: false)
-TemaExperienciaAgroecologica.new(nome: "Sementes").save(validate: false)
-TemaExperienciaAgroecologica.new(nome: "Sistemas Agroflorestais").save(validate: false)
-TemaExperienciaAgroecologica.new(nome: "Manejo da vegetação nativa e agroextrativismo").save(validate: false)
-TemaExperienciaAgroecologica.new(nome: "Sistemas de criação animal").save(validate: false)
-TemaExperienciaAgroecologica.new(nome: "Sistema de Produção Agrícola").save(validate: false)
-TemaExperienciaAgroecologica.new(nome: "Correntes de Agricultura Alternativa").save(validate: false)
-TemaExperienciaAgroecologica.new(nome: "Manejo de pragas, doenças e plantas espontâneas").save(validate: false)
-TemaExperienciaAgroecologica.new(nome: "Manejo dos solos").save(validate: false)
-TemaExperienciaAgroecologica.new(nome: "Acesso a terra, reforma agrária e Agroecologia").save(validate: false)
-TemaExperienciaAgroecologica.new(nome: "Relação com os mercados").save(validate: false)
-TemaExperienciaAgroecologica.new(nome: "Outros").save(validate: false)
+TemaExperienciaAgroecologica.new(nome: "Agricultura Urbana", usuario_id: 1).save!
+TemaExperienciaAgroecologica.new(nome: "Alimentação", usuario_id: 1).save!
+TemaExperienciaAgroecologica.new(nome: "Saúde e plantas medicinais", usuario_id: 1).save!
+TemaExperienciaAgroecologica.new(nome: "Desenvolvimento Rural", usuario_id: 1).save!
+TemaExperienciaAgroecologica.new(nome: "Desenvolvimento Local", usuario_id: 1).save!
+TemaExperienciaAgroecologica.new(nome: "Educação", usuario_id: 1).save!
+TemaExperienciaAgroecologica.new(nome: "Pesquisa Agrícola e Extensão Rural", usuario_id: 1).save!
+TemaExperienciaAgroecologica.new(nome: "Manejo de Recursos Hídricos", usuario_id: 1).save!
+TemaExperienciaAgroecologica.new(nome: "Certificação", usuario_id: 1).save!
+TemaExperienciaAgroecologica.new(nome: "Comercialização", usuario_id: 1).save!
+TemaExperienciaAgroecologica.new(nome: "Processamento e beneficiamento da produção", usuario_id: 1).save!
+TemaExperienciaAgroecologica.new(nome: "Sementes", usuario_id: 1).save!
+TemaExperienciaAgroecologica.new(nome: "Sistemas Agroflorestais", usuario_id: 1).save!
+TemaExperienciaAgroecologica.new(nome: "Manejo da vegetação nativa e agroextrativismo", usuario_id: 1).save!
+TemaExperienciaAgroecologica.new(nome: "Sistemas de criação animal", usuario_id: 1).save!
+TemaExperienciaAgroecologica.new(nome: "Sistema de Produção Agrícola", usuario_id: 1).save!
+TemaExperienciaAgroecologica.new(nome: "Correntes de Agricultura Alternativa", usuario_id: 1).save!
+TemaExperienciaAgroecologica.new(nome: "Manejo de pragas, doenças e plantas espontâneas", usuario_id: 1).save!
+TemaExperienciaAgroecologica.new(nome: "Manejo dos solos", usuario_id: 1).save!
+TemaExperienciaAgroecologica.new(nome: "Acesso a terra, reforma agrária e Agroecologia", usuario_id: 1).save!
+TemaExperienciaAgroecologica.new(nome: "Relação com os mercados", usuario_id: 1).save!
+TemaExperienciaAgroecologica.new(nome: "Outros", usuario_id: 1).save!
 
 
