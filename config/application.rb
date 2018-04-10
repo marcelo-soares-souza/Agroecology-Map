c0890497fefac6fa46e2cfa5548d51ed62b11289
Marcelo Soares Souza <marcelo@riseup.net>
2018-04-09 21:56:50 -0300

    Removed Google, added Mapbox :)

--- config/application.rb
+++ config/application.rb
@@ -2,17 +2,18 @@ require_relative 'boot'
 
 require 'rails/all'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Agroecologia
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.generators do |g|
+      g.template_engine nil 
+      g.test_framework  nil 
+      g.assets  false
+      g.helper false
+      g.stylesheets false
+    end
   end
 end
