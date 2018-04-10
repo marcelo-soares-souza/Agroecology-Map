61dc82cc5d340d97e6d4d6fae13c43fa49ed899c
Marcelo Soares Souza <marcelo@itbio3.org>
2018-04-10 15:52:29 -0300

    Resolve a possible conflict with duplicate Plant name (add scientific name to slug)

--- app/models/planta.rb
+++ app/models/planta.rb
@@ -5,14 +5,22 @@ class Planta < ApplicationRecord
   has_many :safs, through: :saf_plantas
 
   extend FriendlyId
-  friendly_id :nome, use: :slugged
+  friendly_id :slug_candidates, use: :slugged
 
-  validates :nome, presence: true, uniqueness: true
+  validates :nome, presence: true
 
   has_attached_file :imagem, styles: { medium: "360x360>", thumb: "180x180>" }, default_url: "/assets/missing.png"
   validates_attachment_content_type :imagem, content_type: /\Aimage\/.*\z/
 
   protected
+    def slug_candidates
+      [
+        :nome,
+        [:nome, :nome_cientifico],
+        [:nome, :nome_cientifico, :id]
+      ]
+    end
+
     def should_generate_new_friendly_id?
       nome_changed?
     end
