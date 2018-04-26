18796490369c487269294ad3da071714888b3996
Marcelo Soares Souza <marcelo@riseup.net>
2018-04-26 13:02:55 -0300

    Removed Turbolinks, conflicts with Leaflet

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require turbolinks
 //= require rails-ujs
 //= require ckeditor/init
 //= require_tree .
