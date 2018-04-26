50df4f5ebe29578f3df5a6b49ffbc0d99e7eae8c
Marcelo Soares Souza <marcelo@riseup.net>
2018-04-26 12:24:39 -0300

    Turbolinks enabled

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require turbolinks
 //= require rails-ujs
 //= require ckeditor/init
 //= require_tree .
