14be683c845471c7c33bf370c897faa99c918fb8
marcelo.souza <marcelo.souza@basis.com.br>
2020-12-29 16:01:25 -0300

    Minor Fixes and Adjustments in Layout

--- Gemfile
+++ Gemfile
@@ -6,7 +6,7 @@ git_source(:github) do |repo_name|
 end
 
 gem 'rails', '~> 5.2.4.4'
-gem 'pg', '~> 1.1.4'
+gem 'pg', '~> 1.2.3'
 gem 'puma', '~> 3.12'
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 4.1.8'
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,9 @@ usuario_admin = Usuario.new(nome: "Administrador", email: "admin@agroecologia", 
 usuario_admin.skip_confirmation!
 usuario_admin.save!
 
-usuario_itbio3 = Usuario.new(nome: "ITBio3", email: "contact@itbio3.org", password: "itbio3", password_confirmation: "itbio3", admin: "f")
-usuario_itbio3.skip_confirmation!
-usuario_itbio3.save!
+usuario_agroecologia = Usuario.new(nome: "Agroecologia", email: "contact@mapadaagroecologia.org", password: "agroecologia", password_confirmation: "agroecologia", admin: "f")
+usuario_agroecologia.skip_confirmation!
+usuario_agroecologia.save!
 
 TipoOrganizacao.new(nome: "Organização Não Governamental", usuario_id: 1).save!
 TipoOrganizacao.new(nome: "Governamental", usuario_id: 1).save!
