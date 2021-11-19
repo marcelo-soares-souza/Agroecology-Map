0996a98be36772af694c3e97f583023d9c97db13
Marcelo Soares Souza <marcelo@agroecologymap.org>
2021-11-19 15:05:20 -0300

    Adicionando Invisible Captcha em todos os Formulários

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  invisible_captcha only: [:create, :update]
   protect_from_forgery with: :exception
   before_action :store_user_location!, if: :storable_location?
   before_action :configure_permitted_parameters, if: :devise_controller?
