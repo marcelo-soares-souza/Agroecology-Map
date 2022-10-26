31dc1d55c05fd48f2d0029cfa5f1df7c80db82ca
Marcelo Soares Souza <marcelo@agroecologymap.org>
2022-10-26 08:41:50 -0300

    Improvements in Translation in Agroecology Experiences

--- app/controllers/experiencia_agroecologicas_controller.rb
+++ app/controllers/experiencia_agroecologicas_controller.rb
@@ -4,10 +4,10 @@ class ExperienciaAgroecologicasController < ApplicationController
   before_action :set_experiencia_agroecologica, only: %i[show edit update destroy]
   before_action :authenticate_usuario!, only: %i[new edit update destroy]
   before_action lambda {
-                  check_owner ExperienciaAgroecologica.friendly.find(params[:id]).usuario_id
-                }, only: %i[edit update destroy]
+    check_owner ExperienciaAgroecologica.friendly.find(params[:id]).usuario_id
+  }, only: %i[edit update destroy]
   before_action :load_locais, except: %i[index show]
-  before_action :load_tema_experiencia_agroecologicas, except: %i[index show]
+  before_action :load_tema_experiencia_agroecologicas # , except: %i[index show]
   before_action :load_local
 
   # GET /experiencia_agroecologicas
@@ -89,7 +89,31 @@ class ExperienciaAgroecologicasController < ApplicationController
     end
 
     def load_tema_experiencia_agroecologicas
-      @tema_experiencia_agroecologicas = TemaExperienciaAgroecologica.all
+      # @tema_experiencia_agroecologicas = TemaExperienciaAgroecologica.all
+      @tema_experiencia_agroecologicas = {
+        t(:urban_agriculture) => 1,
+        t(:food) => 2,
+        t(:health_and_medicinal_plants) => 3,
+        t(:rural_development) => 4,
+        t(:local_development) => 5,
+        t(:education) => 6,
+        t(:agricultural_research_and_rural_extension) => 7,
+        t(:management_of_water_resources) => 8,
+        t(:certification) => 9,
+        t(:marketing) => 10,
+        t(:processing_and_processing_of_production) => 11,
+        t(:seeds) => 12,
+        t(:agroforestry_systems) => 13,
+        t(:management_of_native_vegetation_and_agro_extractivism) => 14,
+        t(:animal_husbandry_systems) => 15,
+        t(:agricultural_production_system) => 16,
+        t(:alternative_agriculture_currents) => 17,
+        t(:management_of_pests__diseases_and_weeds) => 18,
+        t(:soil_management) => 19,
+        t(:access_to_land_agrarian_reform_and_agroecology) => 20,
+        t(:relationship_with_the_markets) => 21,
+        t(:others) => 22
+      }
     end
 
     def load_local
