81f92156b58b08f049502f1f0c46b55468315e32
Marcelo Soares Souza <marcelo@agroecologymap.org>
2022-10-28 11:48:00 -0300

    Adjustments in Invisible Captcha

--- config/initializers/invisible_captcha.rb
+++ config/initializers/invisible_captcha.rb
@@ -1,14 +1,8 @@
 # frozen_string_literal: true
 
 InvisibleCaptcha.setup do |config|
-  # config.honeypots           << ['more', 'fake', 'attribute', 'names']
-  # config.visual_honeypots    = false
-  # config.timestamp_threshold = 2
-  # config.timestamp_enabled   = true
-  # config.injectable_styles   = false
+  config.timestamp_threshold = 3
+  config.timestamp_enabled = true
   config.spinner_enabled = true
   config.visual_honeypots = true
-  # Leave these unset if you want to use I18n (see below)
-  # config.sentence_for_humans     = 'If you are a human, ignore this field'
-  # config.timestamp_error_message = 'Sorry, that was too quick! Please resubmit.'
 end
