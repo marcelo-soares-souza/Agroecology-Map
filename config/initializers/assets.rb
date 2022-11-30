0f32d57a954d0ae11ef3fa30ec710e7249f83fcf
Marcelo Soares Souza <marcelo@agroecologymap.org>
2022-11-30 11:32:52 -0300

    Updated Sprockets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,12 +13,3 @@ Rails.application.config.assets.paths << Rails.root.join("node_modules")
 Dir.glob("#{Rails.root}/app/assets/images/**/").each do |path|
   Rails.application.config.assets.paths << path
 end
-
-# Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in the app/assets
-# folder are already added.
-# Rails.application.config.assets.precompile += %w( admin.js admin.css )
-Rails.application.config.assets.precompile =  ["*.js", "*.css"]
-Rails.application.config.assets.precompile += %w[ckeditor/*]
-Rails.application.config.assets.precompile += %w[ckeditor/config.js]
-# Rails.application.config.assets.precompile += %w[ckeditor/config.js]
