4af0dcecf42ebb908f7025650e6b1242e2563a68
Marcelo Soares Souza <marcelo@agroecologymap.org>
2022-12-13 08:12:07 -0300

    Changing Catpcha time to prevent new Spam Bots

--- config/initializers/invisible_captcha.rb
+++ config/initializers/invisible_captcha.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 InvisibleCaptcha.setup do |config|
-  config.timestamp_threshold = 4
+  config.timestamp_threshold = 5
   config.timestamp_enabled = true
   config.spinner_enabled = true
   config.visual_honeypots = false
