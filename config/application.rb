65e7769d2450f9cd6ea62e46ceba881947b98d97
Marcelo Soares Souza <marcelo@agroecologymap.org>
2023-10-05 12:04:52 -0300

    feat: Updated Rails 7.1

--- config/application.rb
+++ config/application.rb
@@ -8,15 +8,12 @@ Bundler.require(*Rails.groups)
 
 module Agroecologia
   class Application < Rails::Application
-    # config.load_defaults 5.1
-    config.load_defaults 7.0
-    config.active_record.legacy_connection_handling = false
+    config.load_defaults 7.1
 
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
     config.log_tags  = [:subdomain, :uuid]
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
-    #    config.generators.javascript_engine = :js
 
     config.generators do |g|
       g.template_engine nil
