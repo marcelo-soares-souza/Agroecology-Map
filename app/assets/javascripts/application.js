92a54015a4dc25d730008f2a9f9ba8bfc8654882
Marcelo Soares Souza <marcelo@agroecologymap.org>
2023-11-13 17:17:14 -0300

    fix: send files using trix editor

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require_tree .
 //= require cookies_eu
 //= require trix
+//= require actiontext
 
 $(document).ready(function () {
     $(".alert").fadeTo(2000, 500).slideUp(500, function () {
