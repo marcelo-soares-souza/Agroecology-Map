311be27d7888d5d4020aeb3277accdf973396d62
Marcelo Soares Souza <marcelo@agroecologymap.org>
2023-12-13 12:19:29 -0300

    fix: fix in order of media in gallery

--- app/controllers/midias_controller.rb
+++ app/controllers/midias_controller.rb
@@ -31,9 +31,9 @@ class MidiasController < ApplicationController
       local = Local.friendly.find(params[:local_id])
       experiencia_agroecologica = ExperienciaAgroecologica.where(local_id: local.id).load_async.sort_by(&:updated_at).reverse
       saf = Saf.where(local_id: local.id).load_async.sort_by(&:updated_at).reverse
-      @midias = Midia.where(experiencia_agroecologica:).load_async.sort_by(&:updated_at).reverse
+      @midias = Midia.where(local_id: local.id).load_async.sort_by(&:updated_at).reverse
+      @midias += Midia.where(experiencia_agroecologica:).load_async.sort_by(&:updated_at).reverse
       @midias += Midia.where(saf:).load_async.sort_by(&:updated_at).reverse
-      @midias += Midia.where(local_id: local.id).load_async.sort_by(&:updated_at).reverse
     end
   end
 
