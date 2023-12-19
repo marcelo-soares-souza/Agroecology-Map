764e5771ca49a71a140cc019856eb39c94e4b4f5
Marcelo Soares Souza <marcelo@agroecologymap.org>
2023-12-19 18:24:03 -0300

    feat: added rspec for agroecology experiences controller

---
+++ spec/factories/experiencia_agroecologicas.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+# FILEPATH: /home/marcelo/Development/Pessoal/agroecologymap/spec/factories/experiencia_agroecologicas.rb
+FactoryBot.define do
+    factory :experiencia_agroecologica do
+      nome { "My Experience in Agroecology" }
+    end
+  end
---
+++ spec/factories/tema_experiencia_agroecologicas.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+# FILEPATH: /home/marcelo/Development/Pessoal/agroecologymap/spec/factories/tema_experiencia_agroecologicas.rb
+FactoryBot.define do
+    factory :tema_experiencia_agroecologica do
+      nome { "My Theme" }
+    end
+  end
