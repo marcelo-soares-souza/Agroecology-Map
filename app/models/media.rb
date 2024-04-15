fbd3055e8095f3d1a08387e34c0d006b8758cbde
Marcelo Soares Souza <marcelo@agroecologymap.org>
2024-04-15 18:05:02 -0300

    fix: fix in new media

--- app/models/media.rb
+++ app/models/media.rb
@@ -6,10 +6,11 @@ class Media < ApplicationRecord
   belongs_to :location, required: false
   belongs_to :practice, required: false
   belongs_to :account, required: false
+
   has_one_attached :photo do |attachable|
-    attachable.variant :original, resize_to_limit: [1920, nil]
-    attachable.variant :medium, resize_to_limit: [600, nil]
-    attachable.variant :thumb, resize_to_limit: [300, nil]
+    attachable.variant :original, { resize_to_limit: [1920, nil], convert: :webp, saver: { subsample_mode: "on", strip: true, interlace: true, quality: 85 } }
+    attachable.variant :medium, { resize_to_limit: [600, nil], convert: :webp, saver: { subsample_mode: "on", strip: true, interlace: true, quality: 85 } }
+    attachable.variant :thumb, { resize_to_limit: [300, nil], convert: :webp, saver: { subsample_mode: "on", strip: true, interlace: true, quality: 85 } }
   end
 
   validates :photo, presence: true
