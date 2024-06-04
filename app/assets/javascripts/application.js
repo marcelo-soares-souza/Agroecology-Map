1fd9dad04cd90606f0f3a8065af78a5e5592aba7
Marcelo Soares Souza <marcelo@agroecologymap.org>
2024-06-04 12:21:26 -0300

    feat: added layer Brigades d’actions paysannes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -42,3 +42,37 @@ async function loadLayers(url, title, color) {
 
     return layers;
 }
+
+async function loadBrigadesActionsPaysannes(url, title, color) {
+    const response = await fetch(url);
+    const locations = await response.json();
+    const layers = L.markerClusterGroup();
+
+    const Icon = L.icon({
+        iconUrl: '/assets/leaflet/images/leaf-'+color+'.png',
+        shadowUrl: '/assets/leaflet/images/leaf-shadow.png',
+        iconSize: [38, 95],
+        shadowSize: [50, 64],
+        iconAnchor: [22, 94],
+        shadowAnchor: [4, 62],
+        popupAnchor: [-3, -76]
+    });
+
+    for (let location of locations['features']) {
+        const properties = location['properties']['title'];
+        const latitude = location['geometry']['coordinates'][1];
+        const longitude = location['geometry']['coordinates'][0];
+
+        let url_title = document.createElement('url_title');
+        url_title.innerHTML = properties;
+        const title = url_title.textContent;
+        const url = 'https://www.brigadesactionspaysannes.be/' + url_title.firstChild.attributes[0].nodeValue;
+        
+        let popup = '<strong style="font-size: 12px;">' + title + '</strong><br>';
+        popup += '<strong><a href=" '+url+'" target="_blank">' + title + '</a></strong>';
+
+        layers.addLayer(new L.marker(new L.latLng(latitude, longitude), {icon: Icon}).bindPopup(popup));
+    };
+
+    return layers;
+}
