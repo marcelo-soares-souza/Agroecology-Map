f1f289589cc3dc9b24e0b69630434adbd0f474fb
Marcelo Soares Souza <marcelo@agroecologymap.org>
2024-09-06 15:48:47 -0300

    fix: remove deprecated

--- app/lib/json_web_token.rb
+++ app/lib/json_web_token.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class JsonWebToken
-  JWT_SECRET = Rails.application.secrets.secret_key_base
+  JWT_SECRET = Rails.application.secret_key_base
 
   def self.encode(payload, exp = 8760.hours.from_now)
     payload[:exp] = exp.to_i
